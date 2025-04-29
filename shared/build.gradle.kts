5dc16465949a904ff1d00a0f0318df5dcc0e323f
lanceli <lanceli1001x@hotmail.com>
2025-04-29 14:21:03 +0800

    fix:add skie

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.room)
     alias(libs.plugins.ksp)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.skie)
 }
 android {
     namespace = "com.seraphim.shared"
